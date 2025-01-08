81b899bbfd6b5668d4b77bd64be53e35af37b86f
jhonatanjcdg <j3172224877@gmail.com>
2025-01-08 18:19:05 -0500

    Se terminó la configuración de Swagger

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,7 +1,9 @@
 /* eslint-disable prettier/prettier */
 import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
   constructor(
