6762f8a93b37e14a661860b384ced90cb9776a5a
jhonatanjcdg <j3172224877@gmail.com>
2025-01-08 18:14:12 -0500

    Agrego logger middleware, configuración de typeorm, falta seguir configurando esos dos

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { LoggerGlobal } from './middlewares/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // LoggerGlobal -> registra las apis con la fecha
+  app.use(LoggerGlobal)
+  
+  // Start Swagger configuration
+  const swaggerConfig = new DocumentBuilder()
+  .setTitle('Trading')
+  .setDescription("Api trading")
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build()
+  
+  const document = SwaggerModule.createDocument(app, swaggerConfig)
+  SwaggerModule.setup('api', app, document)
+  // End Swagger configuration
+  
   await app.listen(3000);
 }
 bootstrap();
