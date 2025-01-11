b19c97efdc615c843418e94f864d94eca188df79
jhonatanjcdg <j3172224877@gmail.com>
2025-01-11 12:48:46 -0500

    Se arregló la conexión a la base de datos, listo para el deploy

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -6,17 +6,19 @@ dotenvConfig({ path: '.env' })
 
 const config = {
     type: 'postgres',
-    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT),
+    database: process.env.DB_NAME,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     autoLoadEntities: true,
-    // dropSchema: true,
     synchronize: true,
     logging: true,
     entities: ['dist/**/*.entity{.ts,.js}'],
-    migrations: ['dist/migrations/*{.js,.ts}']
+    migrations: ['dist/migrations/*{.js,.ts}'],
+    ssl: {
+        rejectUnauthorized: false, // Deshabilitar la validación de certificado (solo para desarrollo)
+    },
 }
 
 export default registerAs('typeorm', () => config)
--- src/modules/users/changePassword.dto.ts
+++ src/modules/users/changePassword.dto.ts
@@ -1,7 +1,38 @@
+import { ApiProperty } from "@nestjs/swagger"
+import { IsNotEmpty, IsString, Matches } from "class-validator"
+
 export class ChangePasswordDto{
+    @ApiProperty({
+        description: 'Contraseña actual del usuario',
+        type: 'string',
+        example: 'Test123.'
+    })
+    @IsString()
+    @IsNotEmpty({
+        message: 'La contraseña actual es requerida'
+    })
     currentPassword: string
 
+    @ApiProperty({
+        description: 'Nueva contraseña del usuario',
+        type: 'string',
+        example: 'NewPass123.'
+    })
+    @IsString()
+    @IsNotEmpty()
+    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*.])[A-Za-z\d!@#$%^&*.]{8,15}$/,{
+        message: 'The password must be between 8 and 15 characters long, and include at least one uppercase letter, one lowercase letter, one number, and one special character (!@#$%^&*.)',
+    })
     newPassword: string
 
+    @ApiProperty({
+        description: 'Confirmación de la nueva contraseña',
+        type: 'string',
+        example: 'NewPass123.'
+    })
+    @IsString()
+    @IsNotEmpty({
+        message: 'La confirmación de contraseña es requerida'
+    })
     confirmPassword: string
 }
